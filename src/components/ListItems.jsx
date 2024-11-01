3fc05cd3446869f60f5c26b4641dc8139bc058a4
navin-ghimire <navinghimire143@gmail.com>
2024-11-01 10:48:03 +0545

    ok

--- src/components/ListItems.jsx
+++ src/components/ListItems.jsx
@@ -22,7 +22,7 @@ const ListItems = () => {
      <div className='flex space-x-4 flex-nowrap'>
      {categories.map((category) => {
        return (
-        <div key={category} className='flex-none bg-gray-200 hover:bg-gray-300 duration-300 rounded-xl px-4 py-2 font-medium text-gray-700 cursor-pointer'>{category}</div>
+        <div key={category} className='mb-4 flex-none bg-gray-200 hover:bg-gray-300 duration-300 rounded-xl px-4 py-2 font-medium text-gray-700 cursor-pointer'>{category}</div>
        )
       })}
      </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { ImYoutube2 } from "react-icons/im";
 import { CiSearch } from "react-icons/ci";
@@ -6,7 +6,22 @@ import { IoMdMic } from "react-icons/io";
 import { RiVideoAddLine } from "react-icons/ri";
 import { AiOutlineBell } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (event) => {
+    if (
+      (event?.key === "Enter" || event === "searchButton") &&
+      searchQuery?.length > 0
+    ) {
+      navigate(`/search/${searchQuery}`);
+      setSearchQuery("");
+    }
+  };
+
   return (
     <div className="flex justify-between fixed top-0 w-[100%] bg-white px-6 py-2">
       <div className=" flex items-center space-x-4">
@@ -15,14 +30,24 @@ const Navbar = () => {
       </div>
       <div className="flex w-[35%] items-center">
         <div className="w-[100%] px-3 py-2 border border-gray-400 rounded-l-full">
-          <input type="text" placeholder="Search" className="outline-none" />
+          <input
+            type="text"
+            placeholder="Search"
+            className="outline-none"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyUp={searchQueryHandler}
+            value={searchQuery}
+          />
         </div>
-        <button className="px-4 py-2 border border-gray-400 bg-gray-100 rounded-r-full">
+        <button
+          className="px-4 py-2 border border-gray-400 bg-gray-100 rounded-r-full"
+          onClick={() => searchQueryHandler("searchButton")}
+        >
           <CiSearch size={"24px"} />
         </button>
         <IoMdMic
           size={"42px"}
-          className="ml-3 border rounded-full p-2 cursor-pointer hover:bg-gray-200 duration-200"
+          className="ml-3 border border-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-200 duration-200"
         />
       </div>
       <div className=" flex space-x-5 items-center">
